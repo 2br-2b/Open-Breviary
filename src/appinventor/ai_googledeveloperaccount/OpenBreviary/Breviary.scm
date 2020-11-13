86172abc03be8d69c5da7499af971bce87c89c93
2br-2b <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2020-11-13 10:31:03 -0500

    Initial Commit

---
+++ src/appinventor/ai_googledeveloperaccount/OpenBreviary/Breviary.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"208","Source":"Form","Properties":{"$Name":"Breviary","$Type":"Form","$Version":"27","AboutScreen":"The page with the web view displaying the Office","AccentColor":"&HFF19B6E6","ActionBar":"True","AlignHorizontal":"3","AlignVertical":"2","AppName":"Breviary","PrimaryColor":"&HFFE64A19","PrimaryColorDark":"&HFFAC0800","ShowStatusBar":"False","Theme":"AppTheme.Light.DarkActionBar","Title":"Breviary","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"WebViewer1","$Type":"WebViewer","$Version":"10","Uuid":"1941094995"}]}}
+|#
